26ee69ed138f46346cef2140e8a63885d1441ca6
Chea Sowattana <10wattana@gmail.com>
2018-05-13 16:58:05 +0700

    add ekoDeliveryServiceTest

--- test/ekoDeliveryServiceTest.js
+++ test/ekoDeliveryServiceTest.js
@@ -1,11 +1,16 @@
 /// EkoDeliveryServiceTest
 
-var ekoDeliveryService = require('./ekoDeliveryService');
-var availableRoutes = require('./ekoDeliveryService-2');
-var assert = require('assert');
+var fromCity = ['A', 'A', 'A', 'B', 'C', 'C', 'D', 'E', 'E','F'];
+var toCity = ['B', 'C', 'D', 'E', 'D', 'F', 'E', 'B', 'A','D'];
+var cost = [1,4,10,3,4,2,1,3,2,1];
 
-describe('Testing', function(){
-	it('Should return string', function(){
-		
+var ekoDeliveryService = require('../ekoDeliveryService');
+var availableRoutes = require('../ekoDeliveryService-2');
+const assert = require('chai').assert;
+const eds = require('../eds');
+
+describe('ekoDeliveryService', function(){
+	it('A-B-E should return 4', function(){
+		assert.equal(ekoDeliveryService.getDeliveryCost(fromCity, toCity, cost, 'A-B-E'), 4);
 	});
 });
