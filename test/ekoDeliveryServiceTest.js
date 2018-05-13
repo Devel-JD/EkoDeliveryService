efc12843ae14897a7cedf77cfa118ee2cd002121
Chea Sowattana <10wattana@gmail.com>
2018-05-13 16:44:39 +0700

    npm init

---
+++ test/ekoDeliveryServiceTest.js
@@ -0,0 +1,11 @@
+/// EkoDeliveryServiceTest
+
+var ekoDeliveryService = require('./ekoDeliveryService');
+var availableRoutes = require('./ekoDeliveryService-2');
+var assert = require('assert');
+
+describe('Testing', function(){
+	it('Should return string', function(){
+		
+	});
+});
